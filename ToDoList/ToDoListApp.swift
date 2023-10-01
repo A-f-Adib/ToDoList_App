ef2a0960ebef1786c14fa647c148fd73e235f1a9
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-10-01 16:21:13 +0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by A.f. Adib on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
